ebd660e1be129ff89923fdc26e01b16ae1338d20
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-06-02 10:42:32 -0400

    create answer question functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :questions
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+
+  resources :questions do
+    resources :answers, shallow: true
+  end
+
   get 'login' => 'sessions#new'
   get 'logout' => 'sessions#delete'
   post 'login' => 'sessions#create'
